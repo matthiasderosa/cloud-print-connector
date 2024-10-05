6514d3a243730d8ceb57bc4f9beb597dbda0aebb
matthiasderosa <matthiasbernges@gmx.de>
2024-10-05 22:29:03 +0200

    Update http.go

--- gcp/http.go
+++ gcp/http.go
@@ -17,8 +17,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/google/cloud-print-connector/lib"
-	"github.com/google/cloud-print-connector/log"
+	"github.com/matthiasderosa/cloud-print-connector/lib"
+	"github.com/matthiasderosa/cloud-print-connector/log"
 
 	"golang.org/x/oauth2"
 )
