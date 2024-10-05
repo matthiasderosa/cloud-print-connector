34229b031d40ccba28255171acacede14aeed2e8
matthiasderosa <matthiasbernges@gmx.de>
2024-10-05 22:30:11 +0200

    Update monitor.go

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -12,12 +12,12 @@ import (
 	"fmt"
 	"net"
 
-	"github.com/google/cloud-print-connector/cups"
-	"github.com/google/cloud-print-connector/gcp"
-	"github.com/google/cloud-print-connector/lib"
-	"github.com/google/cloud-print-connector/log"
-	"github.com/google/cloud-print-connector/manager"
-	"github.com/google/cloud-print-connector/privet"
+	"github.com/matthiasderosa/cloud-print-connector/cups"
+	"github.com/matthiasderosa/cloud-print-connector/gcp"
+	"github.com/matthiasderosa/cloud-print-connector/lib"
+	"github.com/matthiasderosa/cloud-print-connector/log"
+	"github.com/matthiasderosa/cloud-print-connector/manager"
+	"github.com/matthiasderosa/cloud-print-connector/privet"
 )
 
 const monitorFormat = `cups-printers=%d
